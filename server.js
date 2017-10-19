0ee8bd2b4956e2a5958738cb26592f1f2280d2d8
Alan Sutherland <ts-alan.sutherland@rakuten.com>
2017-10-19 14:18:58 +0900

    updated conversation

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ var builder = require('botbuilder');
 var BOTFRAMEWORK_APPID = '717cc4bf-d9af-4c18-a2e0-9a43979a6be2';
 var BOTFRAMEWORK_APPPASSWORD = 'NF3Ya65NmMYUT93f1nFjqcc';
 
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+
 // Get secrets from server environment
 var botConnectorOptions = { 
     appId: process.env.BOTFRAMEWORK_APPID, 
@@ -11,6 +18,9 @@ var botConnectorOptions = {
 
 // Create bot
 var connector = new builder.ChatConnector(botConnectorOptions);
+
+// Listen for messages from users 
+server.post('/api/messages', connector.listen());
 
 // Create your bot with a function to receive messages from the user
 // Create bot and default message handler
@@ -88,71 +98,5 @@ bot.dialog('buyButtonClick', [
     }
 ]).triggerAction({ matches: /(buy|add)\s.*shirt/i });
 
-// var msg = new builder.Message(session)
-//     .addAttachment({
-//         contentType: "application/vnd.microsoft.card.adaptive",
-//         content: {
-//             type: "AdaptiveCard",
-//             speak: "<s>Your  meeting about \"Adaptive Card design session\"<break strength='weak'/> is starting at 12:30pm</s><s>Do you want to snooze <break strength='weak'/> or do you want to send a late notification to the attendees?</s>",
-//                body: [
-//                     {
-//                         "type": "TextBlock",
-//                         "text": "Adaptive Card design session",
-//                         "size": "large",
-//                         "weight": "bolder"
-//                     },
-//                     {
-//                         "type": "TextBlock",
-//                         "text": "Conf Room 112/3377 (10)"
-//                     },
-//                     {
-//                         "type": "TextBlock",
-//                         "text": "12:30 PM - 1:30 PM"
-//                     },
-//                     {
-//                         "type": "TextBlock",
-//                         "text": "Snooze for"
-//                     },
-//                     {
-//                         "type": "Input.ChoiceSet",
-//                         "id": "snooze",
-//                         "style":"compact",
-//                         "choices": [
-//                             {
-//                                 "title": "5 minutes",
-//                                 "value": "5",
-//                                 "isSelected": true
-//                             },
-//                             {
-//                                 "title": "15 minutes",
-//                                 "value": "15"
-//                             },
-//                             {
-//                                 "title": "30 minutes",
-//                                 "value": "30"
-//                             }
-//                         ]
-//                     }
-//                 ],
-//                 "actions": [
-//                     {
-//                         "type": "Action.Http",
-//                         "method": "POST",
-//                         "url": "http://foo.com",
-//                         "title": "Snooze"
-//                     },
-//                     {
-//                         "type": "Action.Http",
-//                         "method": "POST",
-//                         "url": "http://foo.com",
-//                         "title": "I'll be late"
-//                     },
-//                     {
-//                         "type": "Action.Http",
-//                         "method": "POST",
-//                         "url": "http://foo.com",
-//                         "title": "Dismiss"
-//                     }
-//                 ]
-//         }
-//     });
+
+
